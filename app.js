22fc11598a556bd73591401a6664112d5e35cc58
Eisqup <eisqup@gmx.de>
2022-10-21 05:16:06 +0200

    Project done

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 let dinosDataFromJson = [];
 
 fetch("./dino.json")
-.then(response => {return response.json()})
-.then(responseData =>{
-    dinosDataFromJson = responseData.Dinos
+    .then(response => { return response.json() })
+    .then(responseData => {
+        dinosDataFromJson = responseData.Dinos
     });
 
 /**
@@ -16,20 +16,20 @@ function main() {
     /**
      * @description Create a Object with information about the Dino
      */
-    function NewDino(dinoData){
+    function NewDino(dinoData) {
 
         /**
         * @description Convert lbs to KG
         */
-        function weightConverter(value){
-            return Number((value/2.2046).toFixed(1));
+        function weightConverter(value) {
+            return Number((value / 2.2046).toFixed(1));
         }
 
         /**
         * @description Convert inches to meter
         */
-        function heightConverter(value){
-            return Number((value*0.0254).toFixed(2));
+        function heightConverter(value) {
+            return Number((value * 0.0254).toFixed(2));
         }
 
         this.name = dinoData.species;
@@ -39,17 +39,17 @@ function main() {
         this.where = dinoData.where;
         this.when = dinoData.when;
         this.fact = dinoData.fact;
-        this.imgsrc = "./images/" +  dinoData.species.toLowerCase() + ".png";
-        }
-    
+        this.imgsrc = "./images/" + dinoData.species.toLowerCase() + ".png";
+    }
+
     // Create dino Objects and add the each dinoObj to a array
-    let dinoObjArray = dinosDataFromJson.map(function(element){
+    let dinoObjArray = dinosDataFromJson.map(function (element) {
         return new NewDino(element)
     });
-    
+
     //Shuffle the array with IIFE
-    dinoObjArray = (function() {
-        for (let i = dinoObjArray.length - 1; i > 0; i--){
+    dinoObjArray = (function () {
+        for (let i = dinoObjArray.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             const temp = dinoObjArray[i];
             dinoObjArray[i] = dinoObjArray[j]
@@ -57,23 +57,23 @@ function main() {
         }
         return dinoObjArray
     })();
-    
+
     /**
     * @description Function to create a human object
     */
-    function humanData (name , height, weight, diet){
-        return{
-            name:name,
-            height:Number((height / 100).toFixed(2)), //convert cm to meter
-            weight:Number(weight),
-            diet:diet,
+    function humanData(name, height, weight, diet) {
+        return {
+            name: name,
+            height: Number((height / 100).toFixed(2)), //convert cm to meter
+            weight: Number(weight),
+            diet: diet,
             imgsrc: "./images/human.png"
         }
     }
 
     // Use IIFE to get human data from form
-    let humanObj = (function(){
-        
+    let humanObj = (function () {
+
         const name = document.getElementById("name").value;
         const height = document.getElementById("height").value;
         const weight = document.getElementById("weight").value;
@@ -83,38 +83,38 @@ function main() {
     })();
 
     // check if form has the right values in it
-    if (humanObj.name.length === 0 || humanObj.height.length === 0 || humanObj.weight.length === 0 ){
+    if (humanObj.name.length === 0 || humanObj.height.length === 0 || humanObj.weight.length === 0) {
         return alert("Please, enter the right values.")
     }
 
     // Create Dino Compare Method 1 with weight from human and dino
-    NewDino.prototype.createFact1 = function(){
-        if (humanObj.weight < this.weight){
+    NewDino.prototype.createFact1 = function () {
+        if (humanObj.weight < this.weight) {
             return "I´m heavier then You with my " + this.weight + " KG."
-        } else if (humanObj.weight > this.weight){
+        } else if (humanObj.weight > this.weight) {
             return "I´m lighter then You with my " + this.weight + " KG."
         } else {
             return "We both have the same weight."
         }
     };
 
-    
+
     // Create Dino Compare Method 2 with hight from human and dino
-    NewDino.prototype.createFact2 = function(){
-        if (humanObj.height < this.height){
+    NewDino.prototype.createFact2 = function () {
+        if (humanObj.height < this.height) {
             return "I´m " + this.height + " m tall so I´m taller then you."
-        } else if (humanObj.height > this.height){
+        } else if (humanObj.height > this.height) {
             return "I´m smaller then You with my " + this.height + " m."
         } else {
             return "We both have the same height"
         }
     };
 
-    
+
     // Create Dino Compare Method 3 with the diet from human and dino
-    NewDino.prototype.createFact3 = function(){
-        let answer = "I`m a "+ (this.diet)+ " and you are " + humanObj.diet + ".";
-        if (this.diet == humanObj.diet){
+    NewDino.prototype.createFact3 = function () {
+        let answer = "I`m a " + (this.diet) + " and you are " + humanObj.diet + ".";
+        if (this.diet == humanObj.diet) {
             answer += " We both like the same!"
         }
         return answer
@@ -135,12 +135,12 @@ function main() {
     */
     let gridContainer = document.getElementById("grid");
 
-    dinoObjArray.forEach(function(element, index){
+    dinoObjArray.forEach(function (element, index) {
         const container = document.createElement("div");
         container.classList.add("grid-item");
         container.title = "Name: " + element.name + "\n\n" +
-                    "Weight: " + element.weight + " KG\n" +
-                    "Height: " + element.height + " m"
+            "Weight: " + element.weight + " KG\n" +
+            "Height: " + element.height + " m"
 
         const header = document.createElement("h3");
         header.innerText = element.name;
@@ -151,26 +151,26 @@ function main() {
         container.appendChild(img);
 
         // create random facts and runs only if not humanObject is active   
-        if (index !== humanPostionInArray) { 
+        if (index !== humanPostionInArray) {
             const factText = document.createElement("p");
 
             // if species = Pigeon display always the fact "all birds are living dinosaurs"
-            if (element.name === "Pigeon"){
+            if (element.name === "Pigeon") {
                 factText.innerText = element.fact;
             } else {
                 const allFactsArray = [
                     element.createFact1(),
-                    element.createFact2(), 
-                    element.createFact3(), 
+                    element.createFact2(),
+                    element.createFact3(),
                     element.fact,
                     "I have lived in " + element.where + ".",
                     "I wandered over the world in the " + element.when + "."
                 ];
                 // add a random fact to innerText from the allFactsArray
-                factText.innerText = allFactsArray[Math.floor(Math.random()* allFactsArray.length)];
+                factText.innerText = allFactsArray[Math.floor(Math.random() * allFactsArray.length)];
             }
             container.appendChild(factText);
-        };       
+        };
         gridContainer.appendChild(container);
     })
 
@@ -182,7 +182,7 @@ function main() {
 // On button click, prepare and display infographic
 const btn = document.querySelector("#btn");
 
-btn.addEventListener("click",function(e){
+btn.addEventListener("click", function (e) {
     e.preventDefault();
     main();
 });
